37170ba6dc570539f8a5340e3bcc95e5084c5f15
idanilt <idan@ilt.co.il>
2020-01-28 23:28:01 +0200

    Trader API init

---
+++ packages/api/src/index.ts
@@ -0,0 +1,6 @@
+export interface TraderService {
+    balance$(balanceRequest: BalanceRequest): Observable<BalanceEvent>;
+    placeOrder(investRequest: InvestRequest): Promise<InvestResponse>;
+    cancelOrder(sellRequest: SellRequest): Promise<SellResponse>;
+    tradeActions$(tradeActionsRequest: TradeActionsRequest): Observable<TradeActionsEvent>;
+}
