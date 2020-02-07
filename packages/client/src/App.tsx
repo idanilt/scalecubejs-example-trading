f4858a23ae7c2850163d9973d27d4ac07af42caf
idanilt <idan@ilt.co.il>
2020-02-08 00:07:23 +0200

    Added react app

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Assets = () => (<div>hello</div>)
+function Tsx() {
+  return (
+      <div>
+        <h1>Assets App</h1>
+        <Assets />
+      </div>
+  );
+}
+
+export default Tsx;
