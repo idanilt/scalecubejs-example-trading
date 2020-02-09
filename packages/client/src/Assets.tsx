b755088f04f06e8584f8ad632d669ded471e8167
idokatz <idok.mail@gmail.com>
2020-02-09 11:22:58 +0200

    add material ui

--- packages/client/src/Assets.tsx
+++ packages/client/src/Assets.tsx
@@ -1,7 +1,22 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 import { Asset } from './Asset';
 import React, { useEffect, useState } from 'react';
 import { marketService } from './marketServiceProxy';
 import { take, toArray } from 'rxjs/operators';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 
 let waitForAssets = 1;
 export const Assets = () => {
@@ -22,20 +37,25 @@ export const Assets = () => {
     };
   }, [waitForAssets]);
 
+  const classes = useStyles();
   return (
-    <table className="table table-bordered">
-      <tbody>
-        <tr>
-          <th>Id</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Last Update</th>
-          <th>Type</th>
-        </tr>
-        {assets.map((i: any) => (
-          <Asset key={i.id} price={i.price} id={i.id} type={i.type} lastUpdate={i.lastUpdate} />
-        ))}
-      </tbody>
-    </table>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Last</TableCell>
+            <TableCell align="right">Type</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {assets.map((i: any) => (
+            <Asset key={i.id} price={i.price} id={i.id} type={i.type} lastUpdate={i.lastUpdate} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
